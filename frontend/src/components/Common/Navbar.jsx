76567c6754b19b29465b2128dcdb3b2b388ec84b
duyphong204 <duyphong242004@gmail.com>
2025-07-19 22:22:41 +0700

    add vercel json

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import {HiOutlineUser,HiOutlineShoppingBag} from 'react-icons/hi'
 import { HiBars3BottomRight } from 'react-icons/hi2'    
 import SearchBar from './SearchBar'
+import CartDrawer from '../Layout/CartDrawer'
+import { IoMdClose } from 'react-icons/io'
 
 const Navbar = () => {
+    const [drawerOpen,setDrawerOpen]=useState(false)
+    const [navDrawerOpen,setNavDrawerOpen]=useState(false)
+    const toggleNavDrawer = ()=>{
+        setNavDrawerOpen(!navDrawerOpen)
+    }
+    const toggleCartDrawer=()=>{
+        setDrawerOpen(!drawerOpen)
+    }
   return (
     <>
         <nav className='container mx-auto flex items-center justify-between py-4 px-6'>
             <div>
                 <Link to='/' className='text-2xl font-medium'>
-                    DNDP
+                    DP-SHOP
                 </Link>
             </div>
             <div className='hidden md:flex space-x-6'>
@@ -24,7 +34,7 @@ const Navbar = () => {
                 <Link to='/profile' className='hover:text-black'>
                     <HiOutlineUser className='h-6 w-6 text-gray-700'/>
                 </Link>
-                <button className='relative hover:text-black'>
+                <button onClick={toggleCartDrawer} className='relative hover:text-black'>
                     <HiOutlineShoppingBag className='h-6 w-6 text-gray-700'/>
                     <span className='absolute -top-1 bg-rabbit-red text-white text-xs rounded-full px-2 py-0.5'>4</span>
                 </button>
@@ -32,11 +42,22 @@ const Navbar = () => {
                 <div className='overflow-hidden'>
                 <SearchBar/>
                 </div>
-                <button className='md:hidden'>
+                <button onClick={toggleNavDrawer} >
                     <HiBars3BottomRight className='h-6 w-6 text-gray-700'/>
                 </button>
             </div>
         </nav>
+        <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer}/>
+        {/* mobile navigation */}
+        <div className={`fixed top-0 left-0 w-3/4 sm:w-1/2 md:w-1/3 h-full
+            bg-white shadow-lg transform transition-transform
+            duration-300 z-50 ${navDrawerOpen ? 'translate-x-0':'-translate-x-full'} `}>
+            <div className='flex justify-end p-4 '>
+                <button onClick={toggleNavDrawer}>
+                    <IoMdClose className='h-6 w-6 text-gray-600'/>
+                </button>
+            </div>
+        </div>
     </>
   )
 }

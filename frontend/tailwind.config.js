c18224406a18c7a7fa201ac2411b898c2d129cdc
duyphong204 <duyphong242004@gmail.com>
2025-08-01 18:36:43 +0700

    fix config taiwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,18 @@
+//** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {
+//       colors: {
+//         "rabbit-red": "rgb(0,0,0)",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {

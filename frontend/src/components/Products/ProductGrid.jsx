ff0d487f271f511495a7ef774f9568c6c3f53d64
duyphong204 <duyphong242004@gmail.com>
2025-09-09 17:40:12 +0700

    fix

--- frontend/src/components/Products/ProductGrid.jsx
+++ frontend/src/components/Products/ProductGrid.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import fallback from '../../../assets/fallback.png'
 const ProductGrid = ({products,loading ,error}) => {
     if(loading){
         return <p>Loading...</p>
@@ -13,10 +13,10 @@ const ProductGrid = ({products,loading ,error}) => {
             <Link key={index} to={`/product/${product._id}`} className="block">
                 <div className="bg-white p-4 rounded-lg">
                     <div className="w-full h-96 mb-4">
-                        <img 
-                        src={product.images[0].url} 
-                        alt={product.images[0].altText || product.name}
-                        className="w-full h-full object-cover rounded-lg" 
+                        <img
+                            src={product.images?.[0]?.url || fallback}
+                            alt={product.images?.[0]?.altText || product.name || "No image"}
+                            className="w-full h-full object-cover rounded-lg"
                         />
                     </div>
                     <h3 className="text-sm mb-2">{product.name}</h3>
